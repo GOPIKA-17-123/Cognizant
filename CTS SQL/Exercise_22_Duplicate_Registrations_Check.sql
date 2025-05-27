38a1070c575f9db06caf2de91d52188e637e4410
GOPIKA-17-123 <gopikargopika12@gmail.com>
2025-05-27 18:39:09 +0530

    Delete CTS SQL directory

--- CTS SQL/Exercise_22_Duplicate_Registrations_Check.sql
+++
@@ -1,9 +0,0 @@
--- Exercise 22: Duplicate Registrations Check
--- Detect if a user has been registered more than once for the same event.
-SELECT 
-    r.user_id, 
-    r.event_id, 
-    COUNT(*) AS registration_count
-FROM Registrations r
-GROUP BY r.user_id, r.event_id
-HAVING COUNT(*) > 1;
